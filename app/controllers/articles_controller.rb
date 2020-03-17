dd192d0ef0c0b2535e193d6889e1c95a58a7d19a
fernandocib <fernandociballero@gmail.com>
2020-03-16 22:57:22 -0300

    Validation added to Article model

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,7 +4,7 @@ class ArticlesController < ApplicationController
   end
 
   def new
-
+    @article = Article.new
   end
 
   def show
@@ -15,6 +15,8 @@ class ArticlesController < ApplicationController
     @article = Article.new(article_params)
     if @article.save
       redirect_to @article
+    else
+      render 'new'
     end
   end
 
