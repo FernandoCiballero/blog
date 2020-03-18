10cb490d5ecfef8f7b005cbaf60ac33b5a354b5b
fernandocib <fernandociballero@gmail.com>
2020-03-18 18:45:06 -0300

    Refactoring articles and comments controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  before_action :find_article, only: [:show, :edit, :update, :destroy]
   def index
     @articles = Article.all.order("created_at DESC")
   end
@@ -7,9 +8,7 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
-  def show
-    @article = Article.find(params[:id])
-  end
+  def show; end
 
   def create
     @article = Article.new(article_params)
@@ -20,12 +19,9 @@ class ArticlesController < ApplicationController
     end
   end
 
-  def edit
-    @article = Article.find(params[:id])
-  end
+  def edit; end
 
   def update
-    @article = Article.find(params[:id])
     if @article.update(article_params)
       redirect_to @article
     else
@@ -34,12 +30,15 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     redirect_to articles_path
   end
 
   private
+  def find_article
+    @article = Article.find(params[:id])
+  end
+
   def article_params
     params.require(:article).permit(:title, :text)
   end
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,18 +1,14 @@
 class CommentsController < ApplicationController
+  before_action :find_comment, except: :create
   def create
     @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
     redirect_to article_path(@article)
   end
 
-  def edit
-    @article = Article.find(params[:article_id])
-    @comment = @article.comments.find(params[:id])
-  end
+  def edit; end
 
   def update
-    @article = Article.find(params[:article_id])
-    @comment = @article.comments.find(params[:id])
     if @comment.update(comment_params)
       redirect_to article_path(@article)
     else
@@ -21,13 +17,17 @@ class CommentsController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:article_id])
-    @comment = @article.comments.find(params[:id])
     @comment.destroy
     redirect_to article_path(@article)
   end
+  
   private
-    def comment_params
-      params.require(:comment).permit(:commenter, :body)
-    end
+  def find_comment
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.find(params[:id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:commenter, :body)
+  end
 end
