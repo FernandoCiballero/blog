4d14dbdba9a6b3cbc158777601d9583cc18b83f0
fernandocib <fernandociballero@gmail.com>
2020-03-19 21:02:16 -0300

    Installing devise gem

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,4 @@
 class CommentsController < ApplicationController
-  http_basic_authenticate_with name: "root", password: "Abc12345", only: [:update, :destroy]
   before_action :find_comment, except: :create
   def create
     @article = Article.find(params[:article_id])
