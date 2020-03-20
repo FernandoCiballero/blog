0609e6a0669e75a57f422fcd8fb5cd331da4c8c9
fernandocib <fernandociballero@gmail.com>
2020-03-20 14:06:32 -0300

    add image url column to articles

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_19_235858) do
+ActiveRecord::Schema.define(version: 2020_03_20_170231) do
 
   create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2020_03_19_235858) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "category_id", null: false
+    t.string "image_url"
     t.index ["category_id"], name: "index_articles_on_category_id"
   end
 
